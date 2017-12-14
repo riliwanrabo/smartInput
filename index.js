06a3ddf6e6cff01a0342e79cc312396ff00f2655
riliwan <riliwan.rabo@gmail.com>
2017-12-14 13:11:31 +0100

    updated dependencies

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var $ = require('./node_modules/jquery/dist/jquery.min.js');
+var accounting = require('./node_modules/accounting-js/dist/accounting.es6.js');
 var smartInput = require('./smartinput.js');
 module.exports = function() {
     return smartInput;
